f8b05860eb6f1da8b85997fb5892415234e0edf8
Lee Lilly <llilly@princeton.edu>
2013-05-29 02:21:25 -0400

    CSS Change

--- themes/st-martins/templates/page.tpl.php
+++ themes/st-martins/templates/page.tpl.php
@@ -76,21 +76,20 @@
   <div id="branding-container">
 
     <?php if ($logo): ?>
+    <div id="name">
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+      <h1 id="site-name">
+      	<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span>St. Martins</span></a>
+      </h1>
+    </div>
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
+      <div id="slogan">
         <?php if ($site_slogan): ?>
           <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-      </div><!-- /#name-and-slogan -->
+      </div><!-- /#slogan -->
     <?php endif; ?>
     
     
